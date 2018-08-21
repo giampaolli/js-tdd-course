4e70e9f313121f2a2b63203cfc391985cfc5db7d
giampaolli <giampaoli2011@gmail.com>
2018-08-21 15:30:46 -0300

    smoke tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,29 @@
 import { expect } from 'chai'
+import { search, searchAlbuns, searchArtists, searchTracks, searchPlaylists } from '../src/main'
 
 describe('Spotify wrapper', () => {
+
+  describe('Smoke tests', () => {
+
+    it('should exist the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exist the searchAlbuns method', () => {
+      expect(searchAlbuns).to.exist;
+    });
+
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
 
 });
